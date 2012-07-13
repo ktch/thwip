59851ba3d2a9867489a66d759755182a4fb028f9
Zach Phillips <z@zach.be>
2012-07-13 14:56:33 -0400

    First commit

---
+++ application/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Thwip.me anywhere</title>
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<label>Enter your Thwip code.</label>
+	<input name="thwip" type="tel">
+</body>
+</html>
